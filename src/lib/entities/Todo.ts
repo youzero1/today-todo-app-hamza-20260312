d83e647af87962295b71109402d79dc296034325
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-03-12 21:12:24 +0500

    Add src/lib/entities/Todo.ts

---
+++ src/lib/entities/Todo.ts
@@ -0,0 +1,38 @@
+import 'reflect-metadata';
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BeforeInsert,
+} from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity('todos')
+export class Todo {
+  @PrimaryColumn({ type: 'varchar' })
+  id!: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  title!: string;
+
+  @Column({ type: 'varchar', nullable: true, default: null })
+  description!: string | null;
+
+  @Column({ type: 'boolean', default: false })
+  completed!: boolean;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+
+  @BeforeInsert()
+  generateId() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
+}
